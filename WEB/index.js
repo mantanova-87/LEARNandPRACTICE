cd4f23459bbd2d31db91da849b5e7bcd9784a44e
mantanova-87 <cdt.mantavyakumar@gmail.com>
2026-08-02 21:22:09 +0530

    Update styles and structure for passenger counter application

--- WEB/index.js
+++ WEB/index.js
@@ -4,3 +4,14 @@ function increment(){
     count=count+1
     document.getElementById("count-el").innerText=String(count)
 }
+var out=document.getElementById("prev").textContent
+function save(){
+    entry=count
+    dash=','
+    out=out+entry+dash
+    document.getElementById("prev").innerText=out
+    count=0
+    //
+    // 
+    document.getElementById("count-el").innerText=count
+}
