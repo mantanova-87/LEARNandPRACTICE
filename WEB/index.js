97ee0518bffb7a851c368cb121424af375813363
mantanova-87 <cdt.mantavyakumar@gmail.com>
2026-08-04 00:23:00 +0530

    Fix conditional logic in num function to correctly handle operator input and add word wrapping in CSS for better layout

--- WEB/index.js
+++ WEB/index.js
@@ -4,10 +4,13 @@ let str3=''
 let out
 function num(id){
     r=String(id)
-    if (r!='sum' || r!='sub' || r!='prd' || r!='div' || r!='eq')
+    if (r!='sum' && r!='sub' && r!='prd' && r!='div' && r!='eq')
     {
         str1+=document.getElementById(r).textContent
         document.getElementById("n1").innerText=str1
     }
-
+    else {
+        str2+=document.getElementById(r).textContent
+        document.getElementById("op").innerText=str2
+    }
 }
