9d13b17b39117c07d5db99cec5c61aa40476f4e7
mantanova-87 <cdt.mantavyakumar@gmail.com>
2026-08-04 00:06:44 +0530

    Refactor HTML structure to add IDs for calculator display elements and improve JavaScript functionality for input handling

--- WEB/index.js
+++ WEB/index.js
@@ -4,7 +4,10 @@ let str3=''
 let out
 function num(id){
     r=String(id)
-    if (r!='sum' || r!='sub' || r!='prd' || 
-    str1+=document.getElementById(r).textContent
-    console.log(str1)
+    if (r!='sum' || r!='sub' || r!='prd' || r!='div' || r!='eq')
+    {
+        str1+=document.getElementById(r).textContent
+        document.getElementById("n1").innerText=str1
+    }
+
 }
